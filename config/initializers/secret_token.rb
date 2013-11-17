5d5e17fae249633751b9f4d147f478c44af95f9d
rasenn <i.know.defeat@gmail.com>
2013-11-17 23:07:30 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheckHpUpdate2::Application.config.secret_key_base = 'cf18a7c991ec604a5505b9fb9ae551d6f3630fb2c4fa13aaa1a779da7b7b36c06615a306e5269c5317bcd2fe838027133ddb4955f75e4c37eb4d1c57d47fff11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CheckHpUpdate2::Application.config.session_store :cookie_store, key: '_check_hp_update2_session'
